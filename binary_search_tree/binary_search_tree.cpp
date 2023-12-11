20dabecd2a3396178bfb6fcca3f8892d7e6346ee
Mohamed-Amr7 <moh.amr777@gmail.com>
2023-12-11 18:26:55 +0200

    Implemented the recursive insert function and the Node getters & setters

--- binary_search_tree/binary_search_tree.cpp
+++ binary_search_tree/binary_search_tree.cpp
@@ -1,8 +1,38 @@
 #include "binary_search_tree.h"
-#include <iostream>
-#include <memory>
 
 using namespace std;
+
+template<class T>
+BST<T>::Node::Node(T value) {
+    data = value;
+    left = nullptr;
+    right = nullptr;
+}
+
+template<class T>
+T BST<T>::Node::getData() {
+    return data;
+}
+
+template<class T>
+unique_ptr<typename BST<T>::Node> BST<T>::Node::getLeft() {
+    return left;
+}
+
+template<class T>
+unique_ptr<typename BST<T>::Node> BST<T>::Node::getRight(){
+    return right;
+}
+
+template<class T>
+void BST<T>::Node::setLeft(unique_ptr<Node> left_ptr) {
+    left = left_ptr;
+}
+
+template<class T>
+void BST<T>::Node::setRight(unique_ptr<Node> right_ptr) {
+    right = right_ptr;
+}
 
 template<class T>
 BST<T>::BST() {
@@ -11,8 +41,20 @@ BST<T>::BST() {
 }
 
 template<class T>
-BST<T>::Node::Node(T value) {
-    data = value;
-    left = nullptr;
-    right = nullptr;
+unique_ptr<typename BST<T>::Node> BST<T>::insert(unique_ptr<Node>& node, T new_value) {
+    if (node == nullptr) {
+        // If the current node is null, create a new node with the given value.
+        node = make_unique<Node>(new_value);
+        return true;
+    }
+
+    if (new_value < node->getData()) {
+        // Recursively insert into the left subtree.
+        node->setLeft(insert(node->getLeft(), new_value));
+    } else {
+        // Recursively insert into the right subtree.
+        node->setRight(insert(node->getRight(), new_value));
+    }
+    return node;
 }
+
--- binary_search_tree/binary_search_tree.h
+++ binary_search_tree/binary_search_tree.h
@@ -9,20 +9,26 @@ template<class T>
 class BST {
 private:
     class Node {
-    public:
+    private:
         T data;
         unique_ptr<Node> left;
         unique_ptr<Node> right;
-
+    public:
         explicit Node(T value);
+        T getData();
+        unique_ptr<Node> getLeft();
+        unique_ptr<Node> getRight();
+
+        void setLeft(unique_ptr<Node> left_ptr);
+        void setRight(unique_ptr<Node> right_ptr);
     };
 
     unique_ptr<Node> root; // Pointer to the root of the BST.
     int node_count = 0; // Number of elements in the BST.
 
-    bool insert(unique_ptr<Node> &node, T new_value);
+    unique_ptr<Node> insert(unique_ptr<Node> &node, T new_value);
 
-    bool remove(unique_ptr<Node> &parent, unique_ptr<Node> &node, T old_value);
+    unique_ptr<Node> remove(unique_ptr<Node> &parent, unique_ptr<Node> &node, T old_value);
 
     bool contains(unique_ptr<Node> &node, T value);
 
