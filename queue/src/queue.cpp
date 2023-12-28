5597c261fa3696d70063d4d1a467c8e0e6f0fb5f
Mohamed-Amr7 <moh.amr777@gmail.com>
2023-12-28 18:26:13 +0200

    Implemented basic queue methods

--- queue/src/queue.cpp
+++ queue/src/queue.cpp
@@ -1,1 +1,39 @@
-#include "queue.h"
+#include "Queue.h"
+
+template <typename T>
+Queue<T>::Queue() : frontPtr(nullptr), backPtr(nullptr) {}
+
+template <typename T>
+T Queue<T>::front() {
+    ensureNotEmpty();
+    return frontPtr->data;
+}
+
+template <typename T>
+T Queue<T>::back() {
+    ensureNotEmpty();
+    return backPtr->data;
+}
+
+template <typename T>
+bool Queue<T>::isEmpty() {
+    return frontPtr == nullptr;
+}
+
+template <typename T>
+void Queue<T>::ensureNotEmpty() {
+    if (isEmpty()) {
+        throw std::invalid_argument("Queue is empty.");
+    }
+}
+template <typename T>
+int Queue<T>::size() {
+    return _size;
+}
+
+template <typename T>
+void Queue<T>::clear() {
+    frontPtr = nullptr;
+    backPtr = nullptr;
+    _size = 0;
+}
